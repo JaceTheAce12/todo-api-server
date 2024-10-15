4d61652acbe7622ac62f42c2d074e503e63b0f29
Jace Randolph <122485977+JaceTheAce12@users.noreply.github.com>
2024-10-14 19:50:46 -0600

    initial commit

---
+++ modules/todos.js
@@ -0,0 +1,39 @@
+let todos = [
+    {
+        todoId: 0,
+        todoText: 'Go to the store',
+        todoComplete: false,
+        category: 'Shopping',
+        dueDate: '01-05-2024'
+    },
+    {
+        todoId: 1,
+        todoText: 'Mow the lawn',
+        todoComplete: true,
+        category: 'Work',
+        dueDate: '06-15-2024'
+    },
+    {
+        todoId: 2,
+        todoText: 'Create my todo project',
+        todoComplete: false,
+        category: 'Projects',
+        dueDate: '09-10-2024'
+    },
+    {
+        todoId: 3,
+        todoText: 'Watch movie with wife',
+        todoComplete: false,
+        category: 'Personal',
+        dueDate: '09-14-2024'
+    },
+    {
+        todoId: 4,
+        todoText: 'Go camping over labor day weekend',
+        todoComplete: false,
+        category: 'Personal',
+        dueDate: '09-01-2024'
+    },
+]
+
+module.exports = todos;
---
+++ server.js
@@ -0,0 +1,91 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const todos = require('./modules/todos');
+
+app.use(express.json());
+
+app.get('/todos', (req, res) => {
+    res.json(todos);
+});
+
+app.post('/todos', (req, res) => {
+    const newTodo = {
+        todoId: todos.length,
+        todoText: req.body.todoText,
+        todoComplete: req.body.todoComplete,
+        category: req.body.category,
+        dueDate: req.body.dueDate
+    };
+    todos.push(newTodo);
+    res.json(newTodo);
+});
+
+app.put('/todos/:todoId', (req, res) => {
+    const todoId = parseInt(req.params.todoId);
+    const todo = todos.find(todo => todo.todoId === todoId);
+
+    todo.todoText = req.body.todoText;
+    todo.todoComplete = req.body.todoComplete;
+    todo.category = req.body.category;
+    todo.dueDate = req.body.dueDate;
+    res.json(todo);
+});
+
+app.delete('/todos/:todoId', (req, res) => {
+    const todoId = parseInt(req.params.todoId);
+    const todoIndex = todos.findIndex(todo => todo.todoId === todoId);
+
+    if (todoIndex > -1) {
+        const deletedTodo = todos.splice(todoIndex, 1);
+        res.json(deletedTodo);
+    }
+});
+
+app.get('/todos/category/:category', (req, res) => {
+    const category = req.params.category;
+    const categoryTodos = todos.filter(todo => todo.category === category);
+    res.json(categoryTodos);
+});
+
+app.get('/categories', (req, res) => {
+    const categories = [...new Set(todos.map(todo => todo.category))];  
+    res.json(categories);
+});
+
+app.post('/categories', (req, res) => {
+    const category = req.body.category;
+    res.json(category);
+});
+
+app.put('/categories/:todoId/:category', (req, res) => {
+    const category = req.params.category;
+    const newCategory = req.body.newCategory;
+    let updated = false;
+
+    todos.forEach(todo => {
+        if (todo.category === category) {
+            todo.category = newCategory;
+            updated = true;
+        }
+    });
+
+    res.json(todos);
+});
+
+app.delete('/categories/:category', (req, res) => {
+    const category = req.params.category;
+
+    todos.forEach(todo => {
+        if (todo.category === category) {
+            todo.category = null; 
+            found = true;
+        }
+    });
+
+    res.json(todos);
+});
+
+app.listen(port, () => {
+    console.log(`The server is running on port ${port}`);
+});
